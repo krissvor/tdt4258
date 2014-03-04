81709dd4a075e10b437a4193a3ffef30b8310840
Imre Francis Banhidi Kerr <imre.kerr@gmail.com>
2014-03-04 16:36:40 +0100

    O3 is for real men

--- ov2_code/sounds.c
+++ ov2_code/sounds.c
@@ -85,23 +85,23 @@ static noise_note_t noise_silence[] =
 
 static square_note_t coin_notes[] = 
   {
-    {.period_begin = 2*NOTE_Bb, .period_end = 2*NOTE_Bb, .octave = 6, .amp_begin = MAX_AMPLITUDE_PER_CHANNEL, .amp_end = MAX_AMPLITUDE_PER_CHANNEL/1.4, .duty_cycle = 50, .duration = 2205},
-    {.period_begin = NOTE_Eb, .period_end = NOTE_Eb, .octave = 6, .amp_begin = MAX_AMPLITUDE_PER_CHANNEL/1.4, .amp_end = MAX_AMPLITUDE_PER_CHANNEL/1.96, .duty_cycle = 50, .duration = 2205},
-    {.period_begin = NOTE_Eb, .period_end = NOTE_Eb, .octave = 6, .amp_begin = MAX_AMPLITUDE_PER_CHANNEL/1.96, .amp_end = MAX_AMPLITUDE_PER_CHANNEL/2.74, .duty_cycle = 50, .duration = 2205},
-    {.period_begin = NOTE_Eb, .period_end = NOTE_Eb, .octave = 6, .amp_begin = MAX_AMPLITUDE_PER_CHANNEL/2.74, .amp_end = MAX_AMPLITUDE_PER_CHANNEL/3.84, .duty_cycle = 50, .duration = 2205},
-    {.period_begin = NOTE_Eb, .period_end = NOTE_Eb, .octave = 6, .amp_begin = MAX_AMPLITUDE_PER_CHANNEL/3.84, .amp_end = MAX_AMPLITUDE_PER_CHANNEL/5.38, .duty_cycle = 50, .duration = 2205},
-    {.period_begin = NOTE_Eb, .period_end = NOTE_Eb, .octave = 6, .amp_begin = MAX_AMPLITUDE_PER_CHANNEL/5.38, .amp_end = MAX_AMPLITUDE_PER_CHANNEL/7.53, .duty_cycle = 50, .duration = 2205},
-    {.period_begin = NOTE_Eb, .period_end = NOTE_Eb, .octave = 6, .amp_begin = MAX_AMPLITUDE_PER_CHANNEL/7.53, .amp_end = MAX_AMPLITUDE_PER_CHANNEL/10.54, .duty_cycle = 50, .duration = 2205},
-    {.period_begin = NOTE_Eb, .period_end = NOTE_Eb, .octave = 6, .amp_begin = MAX_AMPLITUDE_PER_CHANNEL/10.54, .amp_end = MAX_AMPLITUDE_PER_CHANNEL/14.76, .duty_cycle = 50, .duration = 2205},
-    {.period_begin = NOTE_Eb, .period_end = NOTE_Eb, .octave = 6, .amp_begin = MAX_AMPLITUDE_PER_CHANNEL/14.76, .amp_end = MAX_AMPLITUDE_PER_CHANNEL/20.66, .duty_cycle = 50, .duration = 2205},
-    {.period_begin = NOTE_Eb, .period_end = NOTE_Eb, .octave = 6, .amp_begin = MAX_AMPLITUDE_PER_CHANNEL/20.66, .amp_end = MAX_AMPLITUDE_PER_CHANNEL/28.93, .duty_cycle = 50, .duration = 2205},
-    {.period_begin = NOTE_Eb, .period_end = NOTE_Eb, .octave = 6, .amp_begin = MAX_AMPLITUDE_PER_CHANNEL/28.93, .amp_end = MAX_AMPLITUDE_PER_CHANNEL/40.5, .duty_cycle = 50, .duration = 2205}
+    {.period_begin = 2*NOTE_Bb, .period_end = 2*NOTE_Bb, .octave = 5, .amp_begin = MAX_AMPLITUDE_PER_CHANNEL, .amp_end = MAX_AMPLITUDE_PER_CHANNEL/1.4, .duty_cycle = 50, .duration = 4410},
+    {.period_begin = NOTE_Eb, .period_end = NOTE_Eb, .octave = 5, .amp_begin = MAX_AMPLITUDE_PER_CHANNEL/1.4, .amp_end = MAX_AMPLITUDE_PER_CHANNEL/1.96, .duty_cycle = 50, .duration = 4410},
+    {.period_begin = NOTE_Eb, .period_end = NOTE_Eb, .octave = 5, .amp_begin = MAX_AMPLITUDE_PER_CHANNEL/1.96, .amp_end = MAX_AMPLITUDE_PER_CHANNEL/2.74, .duty_cycle = 50, .duration = 4410},
+    {.period_begin = NOTE_Eb, .period_end = NOTE_Eb, .octave = 5, .amp_begin = MAX_AMPLITUDE_PER_CHANNEL/2.74, .amp_end = MAX_AMPLITUDE_PER_CHANNEL/3.84, .duty_cycle = 50, .duration = 4410},
+    {.period_begin = NOTE_Eb, .period_end = NOTE_Eb, .octave = 5, .amp_begin = MAX_AMPLITUDE_PER_CHANNEL/3.84, .amp_end = MAX_AMPLITUDE_PER_CHANNEL/5.38, .duty_cycle = 50, .duration = 4410},
+    {.period_begin = NOTE_Eb, .period_end = NOTE_Eb, .octave = 5, .amp_begin = MAX_AMPLITUDE_PER_CHANNEL/5.38, .amp_end = MAX_AMPLITUDE_PER_CHANNEL/7.53, .duty_cycle = 50, .duration = 4410},
+    {.period_begin = NOTE_Eb, .period_end = NOTE_Eb, .octave = 5, .amp_begin = MAX_AMPLITUDE_PER_CHANNEL/7.53, .amp_end = MAX_AMPLITUDE_PER_CHANNEL/10.54, .duty_cycle = 50, .duration = 4410},
+    {.period_begin = NOTE_Eb, .period_end = NOTE_Eb, .octave = 5, .amp_begin = MAX_AMPLITUDE_PER_CHANNEL/10.54, .amp_end = MAX_AMPLITUDE_PER_CHANNEL/14.76, .duty_cycle = 50, .duration = 4410},
+    {.period_begin = NOTE_Eb, .period_end = NOTE_Eb, .octave = 5, .amp_begin = MAX_AMPLITUDE_PER_CHANNEL/14.76, .amp_end = MAX_AMPLITUDE_PER_CHANNEL/20.66, .duty_cycle = 50, .duration = 4410},
+    {.period_begin = NOTE_Eb, .period_end = NOTE_Eb, .octave = 5, .amp_begin = MAX_AMPLITUDE_PER_CHANNEL/20.66, .amp_end = MAX_AMPLITUDE_PER_CHANNEL/28.93, .duty_cycle = 50, .duration = 4410},
+    {.period_begin = NOTE_Eb, .period_end = NOTE_Eb, .octave = 5, .amp_begin = MAX_AMPLITUDE_PER_CHANNEL/28.93, .amp_end = MAX_AMPLITUDE_PER_CHANNEL/40.5, .duty_cycle = 50, .duration = 4410}
   };
 
 sound_t coin = 
   {
     .note_count = 11,
-    .note_duration = 2205,
+    .note_duration = 4410,
     .sq1_notes = coin_notes,
     .sq2_notes = sq_silence,
     .tri_notes = tri_silence,
@@ -110,23 +110,23 @@ sound_t coin =
 
 static noise_note_t cannon_notes[] =
   {
-    {.amp_begin = MAX_AMPLITUDE_PER_CHANNEL, .amp_end = MAX_AMPLITUDE_PER_CHANNEL, .multiplier = 1, .duration = 2205},
-    {.amp_begin = MAX_AMPLITUDE_PER_CHANNEL/1.4, .amp_end = MAX_AMPLITUDE_PER_CHANNEL/1.4, .multiplier = 1, .duration = 2205},
-    {.amp_begin = MAX_AMPLITUDE_PER_CHANNEL/1.96, .amp_end = MAX_AMPLITUDE_PER_CHANNEL/1.96, .multiplier = 1, .duration = 2205},
-    {.amp_begin = MAX_AMPLITUDE_PER_CHANNEL/2.74, .amp_end = MAX_AMPLITUDE_PER_CHANNEL/2.74, .multiplier = 1, .duration = 2205},
-    {.amp_begin = MAX_AMPLITUDE_PER_CHANNEL/3.84, .amp_end = MAX_AMPLITUDE_PER_CHANNEL/3.84, .multiplier = 1, .duration = 2205},
-    {.amp_begin = MAX_AMPLITUDE_PER_CHANNEL/5.38, .amp_end = MAX_AMPLITUDE_PER_CHANNEL/5.38, .multiplier = 1, .duration = 2205},
-    {.amp_begin = MAX_AMPLITUDE_PER_CHANNEL/7.53, .amp_end = MAX_AMPLITUDE_PER_CHANNEL/7.53, .multiplier = 1, .duration = 2205},
-    {.amp_begin = MAX_AMPLITUDE_PER_CHANNEL/10.54, .amp_end = MAX_AMPLITUDE_PER_CHANNEL/10.54, .multiplier = 1, .duration = 2205},
-    {.amp_begin = MAX_AMPLITUDE_PER_CHANNEL/14.76, .amp_end = MAX_AMPLITUDE_PER_CHANNEL/14.76, .multiplier = 1, .duration = 2205},
-    {.amp_begin = MAX_AMPLITUDE_PER_CHANNEL/20.66, .amp_end = MAX_AMPLITUDE_PER_CHANNEL/20.66, .multiplier = 1, .duration = 2205},
-    {.amp_begin = MAX_AMPLITUDE_PER_CHANNEL/28.93, .amp_end = MAX_AMPLITUDE_PER_CHANNEL/28.93, .multiplier = 1, .duration = 2205},
+    {.amp_begin = MAX_AMPLITUDE_PER_CHANNEL, .amp_end = MAX_AMPLITUDE_PER_CHANNEL, .multiplier = 1, .duration = 4410},
+    {.amp_begin = MAX_AMPLITUDE_PER_CHANNEL/1.4, .amp_end = MAX_AMPLITUDE_PER_CHANNEL/1.4, .multiplier = 1, .duration = 4410},
+    {.amp_begin = MAX_AMPLITUDE_PER_CHANNEL/1.96, .amp_end = MAX_AMPLITUDE_PER_CHANNEL/1.96, .multiplier = 1, .duration = 4410},
+    {.amp_begin = MAX_AMPLITUDE_PER_CHANNEL/2.74, .amp_end = MAX_AMPLITUDE_PER_CHANNEL/2.74, .multiplier = 1, .duration = 4410},
+    {.amp_begin = MAX_AMPLITUDE_PER_CHANNEL/3.84, .amp_end = MAX_AMPLITUDE_PER_CHANNEL/3.84, .multiplier = 1, .duration = 4410},
+    {.amp_begin = MAX_AMPLITUDE_PER_CHANNEL/5.38, .amp_end = MAX_AMPLITUDE_PER_CHANNEL/5.38, .multiplier = 1, .duration = 4410},
+    {.amp_begin = MAX_AMPLITUDE_PER_CHANNEL/7.53, .amp_end = MAX_AMPLITUDE_PER_CHANNEL/7.53, .multiplier = 1, .duration = 4410},
+    {.amp_begin = MAX_AMPLITUDE_PER_CHANNEL/10.54, .amp_end = MAX_AMPLITUDE_PER_CHANNEL/10.54, .multiplier = 1, .duration = 4410},
+    {.amp_begin = MAX_AMPLITUDE_PER_CHANNEL/14.76, .amp_end = MAX_AMPLITUDE_PER_CHANNEL/14.76, .multiplier = 1, .duration = 4410},
+    {.amp_begin = MAX_AMPLITUDE_PER_CHANNEL/20.66, .amp_end = MAX_AMPLITUDE_PER_CHANNEL/20.66, .multiplier = 1, .duration = 4410},
+    {.amp_begin = MAX_AMPLITUDE_PER_CHANNEL/28.93, .amp_end = MAX_AMPLITUDE_PER_CHANNEL/28.93, .multiplier = 1, .duration = 4410},
   };
 
 sound_t cannon =
   {
     .note_count = 11,
-    .note_duration = 2205,
+    .note_duration = 4410,
     .sq1_notes = sq_silence,
     .sq2_notes = sq_silence,
     .tri_notes = tri_silence,
